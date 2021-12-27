f2085efcf149acc78c98227b4e1ca0d2ff28cd75
megharosejayan <megharose15@gmail.com>
2021-12-27 20:12:43 +0530

    show, delete added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,34 @@ class ArticlesController < ApplicationController
     end
 
     def new
+            @article=Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to article_path(@article)
+        if @article.save
+            flash[:notice] = "Article succes"
+            redirect_to article_path(@article)
+        else
+            redirect_to "new", allow_other_host: true
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+
+    def destroy
+
     end
 end
