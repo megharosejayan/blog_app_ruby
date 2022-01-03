e7d84ca90a8ae35fca7e4edaa95ba88fb232d4be
megharosejayan <megharose15@gmail.com>
2022-01-03 22:08:28 +0530

    sessions implemented for login,logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
 
   def show
     @user = User.find(params[:id])
@@ -30,6 +31,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+          session[:user_id] = @user.id
           flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
           redirect_to articles_path
         else
